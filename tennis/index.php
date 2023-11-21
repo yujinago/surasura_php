dc85030ddf68b566c4385a70e11f3435875ce7ae
yujinago <yuji.yuji.0813@gmail.com>
2023-11-21 19:21:44 +0900

    fix_index.php

--- tennis/index.php
+++ tennis/index.php
@@ -1,4 +1,4 @@
-<?php $info = file_get_contents("info.txt"); ?>
+<?php $fp = fopen("info.txt", "r"); ?>
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -11,7 +11,19 @@
     <main role="main" class="container" style="padding:60px 15px 0;">
       <div>
         <h1>お知らせ</h1>
-        <p><?php echo nl2br($info, false); ?></p>
+        <?php
+        if ($fp) {
+          $title = fgets($fp);
+          if ($title) {
+            echo '<p><a href="info.php">' . $title . '</a></p>';
+          } else {
+            echo '<p>お知らせはありません。</p>';
+          }
+          fclose($fp);
+        } else {
+          echo '<p>お知らせはありません。</p>'
+        }
+        ?>
       </div>
     </main>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
