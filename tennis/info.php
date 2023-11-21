7812d5ffd656fd190455e0e8bd94e4345db068e9
yujinago <yuji.yuji.0813@gmail.com>
2023-11-21 19:14:41 +0900

    create_info.php

---
+++ tennis/info.php
@@ -0,0 +1,21 @@
+<?php $info = file_get_contents("info.txt"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>サークルサイト</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  </head>
+  <body>
+    <?php include('navbar.php'); ?>
+    <main role="main" class="container" style="padding:60px 15px 0;">
+      <div>
+        <h1>お知らせ</h1>
+        <p><?php echo nl2br($info, false); ?></p>
+      </div>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
