e440bf22145d6864ac2a30bd857c50163e3d06aa
yujinago <yuji.yuji.0813@gmail.com>
2023-11-22 18:59:35 +0900

    update_navbar.php

--- tennis/navbar.php
+++ tennis/navbar.php
@@ -10,7 +10,10 @@
         <a class="nav-link" href="info.php">お知らせ</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">アルバム</a>
+        <a class="nav-link" href="upload.php">画像アップロード</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="album.php">アルバム</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">掲示板</a>
