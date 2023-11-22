8f9e722b19aca850c4c73cc4943d27d4662f0bbc
yujinago <yuji.yuji.0813@gmail.com>
2023-11-22 18:56:54 +0900

    create_upload.php

---
+++ tennis/upload.php
@@ -0,0 +1,27 @@
+<?php $fp = fopen("info.txt", "r"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>サークルサイト</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  </head>
+  <body>
+    <?php include('navbar.php'); ?>
+    <main role="main" class="container" style="padding:60px 15px 0;">
+      <div>
+        <h1>画像アップロード</h1>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+          <div class="form-group">
+            <label>アップロードファイル</label>
+            <input type="file" name="image" class="form-control-file">
+          </div>
+          <input type="submit" value="アップロードする" class="btn btn-primary">
+        </form>
+      </div>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="/docs/4.5/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
