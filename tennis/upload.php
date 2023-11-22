069c217b65b6b5d3476e2a3812ba775ca047a13f
yujinago <yuji.yuji.0813@gmail.com>
2023-11-22 19:14:34 +0900

    update_upload.php_2

--- tennis/upload.php
+++ tennis/upload.php
@@ -1,4 +1,19 @@
-<?php $fp = fopen("info.txt", "r"); ?>
+<?php
+$msg = null;
+$alert = null;
+
+if (isset($_FILES['image']) && is_uploaded_file($_FILES['image']['tmp_name'])) {
+  $old_name = $_FILES['image']['tmp_name'];
+  $new_name = $_FILES['image']['name'];
+  if (move_uploaded_file($old_name, 'album/' . $new_name)) {
+    $msg = 'アップロードしました。';
+    $alert = 'success';
+  } else {
+    $msg = 'アップロードできませんでした。';
+    $alert = 'danger';
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -11,6 +26,11 @@
     <main role="main" class="container" style="padding:60px 15px 0;">
       <div>
         <h1>画像アップロード</h1>
+        <?php
+        if ($msg) {
+          echo '<div class="alert alert-' . $alert . '" role="alert">' . $msg . '</div>';
+        }
+        ?>
         <form action="upload.php" method="post" enctype="multipart/form-data">
           <div class="form-group">
             <label>アップロードファイル</label>
