cdfa14928fc1d94bbed99077857274e5442a370d
yujinago <yuji.yuji.0813@gmail.com>
2023-11-22 19:22:30 +0900

    update_upload.php_3

--- tennis/upload.php
+++ tennis/upload.php
@@ -4,7 +4,23 @@ $alert = null;
 
 if (isset($_FILES['image']) && is_uploaded_file($_FILES['image']['tmp_name'])) {
   $old_name = $_FILES['image']['tmp_name'];
-  $new_name = $_FILES['image']['name'];
+  $new_name = date("YmdHis");
+  $new_name .= mt_rand();
+  $size = getimagesize($_FILES['image']['tmp_name']);
+  switch ($size[2]) {
+    case IMAGETYPE_JPEG:
+      $new_name .= '.jpg';
+      break;
+    case IMAGETYPE_GIF:
+      $new_name .= '.gif';
+      break;
+    case IMAGETYPE_PNG:
+      $new_name .= '.png';
+      break;
+    default:
+      header('Location: upload.php');
+      exit();
+  }
   if (move_uploaded_file($old_name, 'album/' . $new_name)) {
     $msg = 'アップロードしました。';
     $alert = 'success';
