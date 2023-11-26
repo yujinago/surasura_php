bec427f01f4f3eaa6a43918bae9f55edba0a0697
yujinago <yuji.yuji.0813@gmail.com>
2023-11-26 17:13:52 +0900

    add_db_write.php

--- tennis/write.php
+++ tennis/write.php
@@ -1,4 +1,6 @@
 <?php
+require_once __DIR__ . '/../../tennis_config.php';
+
 $name = $_POST['name'];
 $title = $_POST['title'];
 $body = $_POST['body'];
@@ -14,4 +16,20 @@ if (!preg_match("/^[0-9]{4}$/", $pass)) {
   exit();
 }
 
+try {
+  $db = new PDO($dsn, $user, $password);
+  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $stmt = $db->prepare('INSERT INTO bbs (name, title, body, date, pass) VALUES (:name, :title, :body, now(), :pass)');
+  $stmt->bindparam(':name', $name, PDO::PARAM_STR);
+  $stmt->bindparam(':title', $title, PDO::PARAM_STR);
+  $stmt->bindparam(':body', $body, PDO::PARAM_STR);
+  $stmt->bindparam(':pass', $pass, PDO::PARAM_STR);
+  $stmt->execute();
+  $db = null;
+
+  header('Location: bbs.php');
+  exit();
+} catch (PDOException $e) {
+  exit('エラー：' . htmlspecialchars($e->getMessage(), ENT_QUOTES) . '<br>');
+}
 ?>
