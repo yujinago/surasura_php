e89505fd7971f8e180f36087aeca876a12869831
yujinago <yuji.yuji.0813@gmail.com>
2023-11-26 18:04:09 +0900

    create_delete.php

---
+++ tennis/delete.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/../../../tennis_config.php';
+
+$id = intval($_POST['id']);
+$pass = $_POST['pass'];
+
+if ($id == '' || $pass == '') {
+  header('Location: bbs.php');
+  exit();
+}
+
+try {
+  $db = new PDO($dsn, $user, $password);
+  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $stmt = $db->prepare("DELETE FROM bbs WHERE id = :id AND pass = :pass");
+  $stmt->bindParam(':id', $id, PDO::PARAM_INT)
+  $stmt->bindParam(':pass', $pass, PDO::PARAM_STR)
+  $stmt->execute();
+} catch (PDOException $e) {
+  exit('エラー：' . htmlspecialchars($e->getMessage(), ENT_QUOTES) . '<br>');
+}
+header('Location: bbs.php');
+exit();
+?>
