42aee3753e354d1423848a493a91a712bdadb082
yujinago <yuji.yuji.0813@gmail.com>
2023-11-26 17:57:17 +0900

    add_delete_bbs.php

--- tennis/bbs.php
+++ tennis/bbs.php
@@ -62,8 +62,13 @@ try {
               </p>
             </div>
             <div class="card-footer">
-              <?php echo $row['name'] ?>
-              (<?php echo $row['date'] ?>)
+              <form action="delete.php" method="post" class="form-inline">
+                <?php echo $row['name'] ?>
+                (<?php echo $row['date'] ?>)
+                <input type="hidden" name="id" value="<?php echo $row['id'] ?>">
+                <input type="text" name="pass" placeholder="削除パスワード" class="form-control">
+                <input type="submit" value="削除" class="btn btn-secondary">
+              </form>
             </div>
           </div>
           </hr>
