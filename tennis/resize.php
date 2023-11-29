4f0c131d3c28eae2f2d7abcd879a9d5b85f7ad05
yujinago <yuji.yuji.0813@gmail.com>
2023-11-29 12:29:55 +0900

    add_jpeg

--- tennis/resize.php
+++ tennis/resize.php
@@ -9,6 +9,11 @@ $new_height = $rate * $height;
 $canvas = imagecreatetruecolor($new_width, $new_height);
 
 switch(exif_imagetype($_FILES['file']['tmp_name'])) {
-  
+  // JPEG
+  case IMAGETYPE_JPEG:
+    $image = imagecreatefromjpeg($_FILES['file']['tmp_name']);
+    imagecopyresampled($canvas, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+    imagejpeg($canvas, 'images/new_image.jpg');
+    break;
 }
 ?>
